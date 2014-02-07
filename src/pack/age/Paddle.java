47342efb36a9f0aa826ecb20355956c7b3de55d1
unknown <Admin@KCV-INLABA03C78.kicvi.ldsb.edu>
2014-02-07 18:38:14 -0500

    i git commit

--- src/pack/age/Paddle.java
+++ src/pack/age/Paddle.java
@@ -3,11 +3,15 @@
  * and open the template in the editor.
  */
 package pack.age;
+import java.awt.Graphics;
 
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 /**
  *
  * @author Admin
  */
-public class Paddle {
-    
+public class Paddle{
+
 }
+
--- src/pack/age/Pong.java
+++ src/pack/age/Pong.java
@@ -8,6 +8,8 @@ import java.awt.*;
 import java.awt.event.WindowEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author Admin
@@ -15,10 +17,14 @@ import javax.swing.JFrame;
 public class Pong
 {
     JFrame frame;
+    JButton button;
     public Pong()
     {
         frame = new JFrame();
+        button = new JButton("40% off Bananas");
+        button.setSize(300,300);
         frame.setSize(500, 500);
+        frame.add(button);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
